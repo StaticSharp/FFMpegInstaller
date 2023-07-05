f330e0ec70db2135ba9cdadf4a5bd7b0d0a1f4b2
pavel.zybenkov <pavel.zybenkov@antilatency.com>
2023-07-05 13:36:43 +0300

    FFMpegPath, FFProbePath, FFPlayPath

--- FFMpegInstaller.cs
+++ FFMpegInstaller.cs
@@ -8,4 +8,10 @@ public static class FFMpegInstaller
     public static readonly string InstallationDirectory = 
         Path.GetFullPath(Path.Combine("ffmpeg", "bin"),
             Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
+    public static readonly string FFMpegPath = Path.Combine(InstallationDirectory, "ffmpeg.exe");
+    
+    public static readonly string FFProbePath = Path.Combine(InstallationDirectory, "ffprobe.exe");
+    
+    public static readonly string FFPlayPath = Path.Combine(InstallationDirectory, "ffplay.exe");
 }
